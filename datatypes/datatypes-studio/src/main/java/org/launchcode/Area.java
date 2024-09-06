1eb9e6b50ad9cff3c627732efa4e2439358d43a6
Douglas Imler <Douglas.Imler@gmail.com>
2024-09-05 20:10:31 -0500

    Studio and HW

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -4,8 +4,18 @@ import java.util.Scanner;
 public class Area {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.println("What is the radius of your favorite circle?: ");
-        double radius = input.nextDouble();
+        double radius = 0;
+        do {
+            try {
+                System.out.println("What is the radius of your favorite circle?: ");
+                radius = input.nextDouble();
+            }
+            catch (Exception e)
+            {
+                System.out.println("Wrong data type you naughty monkey");
+                break;
+            }
+        } while (radius == 0 || radius < 0);
         double area = Circle.getArea(radius);
         System.out.println("The radius of your circle is: " + area);
     }
